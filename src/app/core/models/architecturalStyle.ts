aac23a021a387030401fd9b3b8a144a7d981e938
Nadzeya <primisen17@gmail.com>
2024-08-27 12:52:45 +0200

    Updated style of architectural-style-list page

--- src/app/core/models/architecturalStyle.ts
+++ src/app/core/models/architecturalStyle.ts
@@ -1,8 +1,12 @@
 import { Attribute } from "./attribute";
+import { Image } from "./image";
 
 export interface ArchitecturalStyle{
     id: number;
     name: string;
     description: string;
     attributes: Attribute [];
+    shortDescription: string;
+    yearsActive: string;
+    demonstrativeImage: Image;
 }
