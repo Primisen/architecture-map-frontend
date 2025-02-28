cc54807cab8046b62a11dd66870580f0a47f5a69
Nadzeya <n.gayduk@itransition.com>
2025-02-28 12:58:59 +0100

    Added @ngx-translate/core library for localization

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,49 +1,51 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { Routes, RouterModule, TitleStrategy } from '@angular/router';
-import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { ConstructionComponent } from './features/construction/construction.component';
-import { HomeComponent } from './features/home/home.component';
-import { InfiniteScrollModule } from 'ngx-infinite-scroll';
-import { NgxMasonryModule } from 'ngx-masonry';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NgxGalleryModule } from '@rybos/ngx-gallery';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { SimilarConstructionComponent } from './features/similar-construction/similar-construction.component';
-import { ConstructionImageMasonryComponent } from './shared/construction-image-masonry/construction-image-masonry.component';
-import { ArchitectComponent } from './features/architect/architect.component';
-import { ArchitecturalStyleLinkComponent } from './features/architectural-style-link/architectural-style-link.component';
-import { ArchitecturalStyleComponent } from './features/architectural-style/architectural-style.component';
-import { ArchitectLinkComponent } from './features/architect-link/architect-link.component';
-import { ArchitecturalStyleListComponent } from './features/architectural-style-list/architectural-style-list.component';
-import { SourceListComponent } from './features/source-list/source-list.component';
-import { ArchitectListComponent } from './features/architect-list/architect-list.component';
-import { ContactsComponent } from './features/contacts/contacts.component';
-import { NavComponent } from './core/components/nav/nav.component';
-import { LightgalleryModule } from 'lightgallery/angular';
-import { PageNotFoundComponent } from './core/components/page-not-found/page-not-found.component';
-import { CustomPageTitleStrategy } from './core/services/custom-page-title-strategy';
-import { ConstructionTitleResolver } from './core/services/construction-page-title-resolve';
-import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
-import { SearchingWithFiltersComponent } from './features/searching-with-filters/searching-with-filters.component';
+import { NgModule } from '@angular/core'
+import { BrowserModule } from '@angular/platform-browser'
+import { Routes, RouterModule, TitleStrategy } from '@angular/router'
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http'
+import { AppRoutingModule } from './app-routing.module'
+import { AppComponent } from './app.component'
+import { ConstructionComponent } from './features/construction/construction.component'
+import { HomeComponent } from './features/home/home.component'
+import { InfiniteScrollModule } from 'ngx-infinite-scroll'
+import { NgxMasonryModule } from 'ngx-masonry'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { NgxGalleryModule } from '@rybos/ngx-gallery'
+import { FormsModule, ReactiveFormsModule } from '@angular/forms'
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome'
+import { SimilarConstructionComponent } from './features/similar-construction/similar-construction.component'
+import { ConstructionImageMasonryComponent } from './shared/construction-image-masonry/construction-image-masonry.component'
+import { ArchitectComponent } from './features/architect/architect.component'
+import { ArchitecturalStyleLinkComponent } from './features/architectural-style-link/architectural-style-link.component'
+import { ArchitecturalStyleComponent } from './features/architectural-style/architectural-style.component'
+import { ArchitectLinkComponent } from './features/architect-link/architect-link.component'
+import { ArchitecturalStyleListComponent } from './features/architectural-style-list/architectural-style-list.component'
+import { SourceListComponent } from './features/source-list/source-list.component'
+import { ArchitectListComponent } from './features/architect-list/architect-list.component'
+import { ContactsComponent } from './features/contacts/contacts.component'
+import { NavComponent } from './core/components/nav/nav.component'
+import { LightgalleryModule } from 'lightgallery/angular'
+import { PageNotFoundComponent } from './core/components/page-not-found/page-not-found.component'
+import { CustomPageTitleStrategy } from './core/services/custom-page-title-strategy'
+import { ConstructionTitleResolver } from './core/services/construction-page-title-resolve'
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner'
+import { SearchingWithFiltersComponent } from './features/searching-with-filters/searching-with-filters.component'
+import { TranslateModule } from '@ngx-translate/core'
 
 const appRoutes: Routes = [
-  { path: '', component: HomeComponent, title: 'Architecture Map' },
-  { path: 'constructions/:constructionId/:imageId', component: ConstructionComponent, title: ConstructionTitleResolver },
-  { path: 'constructions/:constructionId', component: ConstructionComponent, title: ConstructionTitleResolver },
-  { path: 'architectural-styles/:id', component: ArchitecturalStyleComponent },
-  { path: 'architects/:id', component: ArchitectComponent },
-  { path: 'architectural-styles', component: ArchitecturalStyleListComponent, title: 'Архітэктурныя стылі' },
-  { path: 'sources', component: SourceListComponent, title: 'Крыніцы' },
-  { path: 'architects', component: ArchitectListComponent, title: 'Архітэктары' },
-  { path: 'contacts', component: ContactsComponent, title: 'Адваротная сувязь' },
-  { path: '**', component: PageNotFoundComponent, title: 'Старонка не знойдзена'}
-];
+    { path: '', component: HomeComponent, title: 'Architecture Map' },
+    { path: 'constructions/:constructionId/:imageId', component: ConstructionComponent, title: ConstructionTitleResolver },
+    { path: 'constructions/:constructionId', component: ConstructionComponent, title: ConstructionTitleResolver },
+    { path: 'architectural-styles/:id', component: ArchitecturalStyleComponent },
+    { path: 'architects/:id', component: ArchitectComponent },
+    { path: 'architectural-styles', component: ArchitecturalStyleListComponent, title: 'Архітэктурныя стылі' },
+    { path: 'sources', component: SourceListComponent, title: 'Крыніцы' },
+    { path: 'architects', component: ArchitectListComponent, title: 'Архітэктары' },
+    { path: 'contacts', component: ContactsComponent, title: 'Адваротная сувязь' },
+    { path: '**', component: PageNotFoundComponent, title: 'Старонка не знойдзена' },
+]
 
-@NgModule({ declarations: [
+@NgModule({
+    declarations: [
         AppComponent,
         ConstructionComponent,
         HomeComponent,
@@ -61,9 +63,9 @@ const appRoutes: Routes = [
         PageNotFoundComponent,
         SearchingWithFiltersComponent,
     ],
-    bootstrap: [
-        AppComponent
-    ], imports: [BrowserModule,
+    bootstrap: [AppComponent],
+    imports: [
+        BrowserModule,
         RouterModule.forRoot(appRoutes),
         AppRoutingModule,
         InfiniteScrollModule,
@@ -74,8 +76,9 @@ const appRoutes: Routes = [
         FontAwesomeModule,
         LightgalleryModule,
         MatProgressSpinnerModule,
-        ReactiveFormsModule], providers: [
-        { provide: TitleStrategy, useClass: CustomPageTitleStrategy },
-        provideHttpClient(withInterceptorsFromDi()),
-    ] })
-export class AppModule { }
+        ReactiveFormsModule,
+        TranslateModule,
+    ],
+    providers: [{ provide: TitleStrategy, useClass: CustomPageTitleStrategy }, provideHttpClient(withInterceptorsFromDi())],
+})
+export class AppModule {}
